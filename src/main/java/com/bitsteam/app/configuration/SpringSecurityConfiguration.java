94281e60b6dc4d0a582f257661d530c38484a3cd
Nicolas <nicosan12@hotmail.com>
2024-10-20 00:33:24 -0500

    Cors added

--- src/main/java/com/bitsteam/app/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/bitsteam/app/configuration/SpringSecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.bitsteam.app.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -13,6 +17,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
@@ -35,24 +44,44 @@ public class SpringSecurityConfiguration {
 	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
 				.authorizeHttpRequests(authz ->
-				authz
-				//public resources added
-				.requestMatchers(HttpMethod.POST, "/api/user/register").permitAll()
-				.requestMatchers(HttpMethod.GET, "/api/user").permitAll()
+					authz
+					//public resources added
+					.requestMatchers(HttpMethod.POST, "/api/user/register").permitAll()
+					.requestMatchers(HttpMethod.GET, "/api/user").permitAll()
+					
+					//rules added
+					//.requestMatchers(HttpMethod.POST, "/api/user").hasRole("ADMIN")
+					//.requestMatchers(HttpMethod.POST, "/api/gender").hasRole("ADMIN")
+					//.requestMatchers(HttpMethod.PUT, "/api/gender/{id}").hasRole("ADMIN")
+					//.requestMatchers(HttpMethod.DELETE, "/api/gender/{id}").hasRole("ADMIN")
 				
-				//rules added
-				//.requestMatchers(HttpMethod.POST, "/api/user").hasRole("ADMIN")
-				//.requestMatchers(HttpMethod.POST, "/api/gender").hasRole("ADMIN")
-				//.requestMatchers(HttpMethod.PUT, "/api/gender/{id}").hasRole("ADMIN")
-				//.requestMatchers(HttpMethod.DELETE, "/api/gender/{id}").hasRole("ADMIN")
-				
-						.anyRequest().authenticated())	
+				.anyRequest().authenticated())	
 				
 				.addFilter(new JwtAuthenticationFilter(this.authenticationManager()))
 				.addFilter(new JwtValidationFilter(this.authenticationManager()))
 				.csrf(config -> config.disable())
+				.cors(cors -> cors.configurationSource(configurationSource()))
 				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.build();
+	}
+	
+	@Bean
+	CorsConfigurationSource configurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOriginPatterns(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		config.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+	
+	@Bean
+	FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<>(new CorsFilter(this.configurationSource()));
+		corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return corsFilter;
 	}
 
 }
--- src/main/java/com/bitsteam/app/controllers/UserController.java
+++ src/main/java/com/bitsteam/app/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ import com.bitsteam.app.services.UserService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(originPatterns = "*")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
